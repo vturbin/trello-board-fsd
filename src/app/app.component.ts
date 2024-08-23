e6afa2fe110d69258bb9fd06aaae6bd101eca323
Valentin Turbins <v.turbin@hotmail.com>
2024-08-23 19:23:14 +0200

    added route in app. set up language and get session on app initialization. Set up app component with router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextFieldComponent } from '../shared/ui/text-field/text-field.component';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../shared/ui/button/button.component';
 import { LogoComponent } from '../shared/ui/logo/logo.component';
-import { PrimeIcons } from 'primeng/api';
 import { IconComponent } from '../shared/ui/icon/icon.component';
 import { SpinnerComponent } from '../shared/ui/spinner/spinner.component';
 import { PageSpinnerComponent } from '../shared/ui/page-spinner/page-spinner.component';
@@ -20,9 +14,19 @@ import { ImageSelectComponent } from '../shared/ui/image-select/image-select.com
 import { ModalComponent } from '../shared/ui/modal/modal.component';
 import { ButtonModule } from 'primeng/button';
 import { MultiselectComponent } from '../shared/ui/multiselect/multiselect.component';
-import { getAvatarUrl } from '../entities/user';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { PasswordComponent } from '../shared/ui/password/password.component';
+import { ToastModule } from 'primeng/toast';
+import { Store } from '@ngrx/store';
+import {
+  loadSession,
+  selectCurrentSession,
+  selectIsLoading,
+  Session,
+} from '@entities/session';
+import { Observable, of } from 'rxjs';
+import { PublicHeaderComponent } from './layouts/header/public-header/public-header.component';
+import { PrivateHeaderComponent } from './layouts/header/private-header/private-header.component';
 
 @Component({
   selector: 'app-root',
@@ -45,64 +49,98 @@ import { PasswordComponent } from '../shared/ui/password/password.component';
     MultiselectComponent,
     TranslateModule,
     PasswordComponent,
+    PublicHeaderComponent,
+    PrivateHeaderComponent,
+    ToastModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  public cities: { name: string; code: string }[] = [
-    { name: 'New York', code: 'NY' },
-    { name: 'Rome', code: 'RM' },
-    { name: 'London', code: 'LDN' },
-    { name: 'Istanbul', code: 'IST' },
-    { name: 'Paris', code: 'PRS' },
-  ];
+  protected pageLoading$: Observable<boolean> = of(true);
+  protected session$: Observable<Session | undefined>;
+
+  constructor(
+    private translateService: TranslateService,
+    private store: Store,
+  ) {
+    this.store.dispatch(loadSession());
+    this.pageLoading$ = this.store.select(selectIsLoading);
+    this.session$ = this.store.select(selectCurrentSession);
+  }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.pageLoading = false;
-      console.log(this.pageLoading);
-    }, 1000);
-    this.loginFormGroup
-      .get('selectedCity')
-      ?.valueChanges.subscribe(value => console.log(value));
-  }
-  title = 'trello-board-fsd';
-
-  public avatarUrl = getAvatarUrl;
-
-  public readonly Icons = PrimeIcons;
-
-  public isModalVisible: boolean = false;
-
-  public pageLoading = true;
-
-  public images = Array.from({ length: 8 }, (_, i) => i + 1);
-
-  public loginFormGroup = new FormGroup({
-    login: new FormControl(null, {
-      validators: [Validators.required, Validators.minLength(2)],
-    }),
-    password: new FormControl(null, Validators.required),
-    selectedCity: new FormControl<{ name: string; code: string } | null>(null),
-    selectedCities: new FormControl<{ name: string; code: string }[] | null>([
-      { name: 'Istanbul', code: 'IST' },
-    ]),
-  });
-
-  public buttonPressed(): void {
-    console.log('button pressed');
+    const languageTag = this.getBrowserLanguage();
+    const language = languageTag.split('-')[0]; //"en","de", etc.
+    const defaultLanguage = this.getDefaultLanguage(language);
+    this.translateService.setDefaultLang(defaultLanguage);
   }
 
-  public handleImageChange(image: string | number): void {
-    console.log(image);
+  getBrowserLanguage() {
+    return navigator.language;
   }
 
-  showModal() {
-    this.isModalVisible = true;
+  getDefaultLanguage(language: string) {
+    const availableLanguages = ['en', 'de'];
+    if (availableLanguages.includes(language)) {
+      return language;
+    } else {
+      return 'en';
+    }
   }
+  // public cities: { name: string; code: string }[] = [
+  //   { name: 'New York', code: 'NY' },
+  //   { name: 'Rome', code: 'RM' },
+  //   { name: 'London', code: 'LDN' },
+  //   { name: 'Istanbul', code: 'IST' },
+  //   { name: 'Paris', code: 'PRS' },
+  // ];
 
-  closeModal() {
-    this.isModalVisible = false;
-  }
+  // ngOnInit(): void {
+  //   setTimeout(() => {
+  //     this.pageLoading = false;
+  //     console.log(this.pageLoading);
+  //   }, 1000);
+  //   this.loginFormGroup
+  //     .get('selectedCity')
+  //     ?.valueChanges.subscribe(value => console.log(value));
+  // }
+  // title = 'trello-board-fsd';
+
+  // public avatarUrl = getAvatarUrl;
+
+  // public readonly Icons = PrimeIcons;
+
+  // public isModalVisible: boolean = false;
+
+  // public pageLoading = true;
+
+  // public images = Array.from({ length: 8 }, (_, i) => i + 1);
+
+  // public loginFormGroup = new FormGroup({
+  //   login: new FormControl(null, {
+  //     validators: [Validators.required, Validators.minLength(2)],
+  //   }),
+  //   password: new FormControl(null, Validators.required),
+  //   selectedCity: new FormControl<{ name: string; code: string } | null>(null),
+  //   selectedCities: new FormControl<{ name: string; code: string }[] | null>([
+  //     { name: 'Istanbul', code: 'IST' },
+  //   ]),
+  // });
+
+  // public buttonPressed(): void {
+  //   console.log('button pressed');
+  // }
+
+  // public handleImageChange(image: string | number): void {
+  //   console.log(image);
+  // }
+
+  // showModal() {
+  //   this.isModalVisible = true;
+  // }
+
+  // closeModal() {
+  //   this.isModalVisible = false;
+  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ROUTER_PATHS } from '@shared/constants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ROUTER_PATHS.SIGN_IN,
+    loadComponent: () =>
+      import('../pages/sign-in').then(m => m.SignInPageComponent),
+  },
+  {
+    path: 'board',
+    loadComponent: () =>
+      import('../pages/boards').then(m => m.BoardsPageComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
